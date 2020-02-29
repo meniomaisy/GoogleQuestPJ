968e5bc213426f42e71ce3f1db546a1249509a47
AnavShapira <57531262+AnavShapira@users.noreply.github.com>
2020-02-29 18:59:14 +0200

    final changes 3

--- amen_script.py
+++ amen_script.py
@@ -43,90 +43,61 @@ def _get_ids(tokens, tokenizer, max_seq_length):
     input_ids = token_ids + [0] * (max_seq_length-len(token_ids))
     return input_ids
 
-def _trim_input(title, question, answer, sub_category, q_class, q_keywords, query, max_sequence_length, 
-                t_max_len=30, q_max_len=111, a_max_len=111, s_c_max_len=30, q_c_max_len=10, q_k_max_len=106, qu_max_len=106):
+def _trim_input(title, answer, sub_category, q_class, q_keywords, max_sequence_length, 
+                t_max_len=150, a_max_len=196, s_c_max_len=25, q_c_max_len=10, q_k_max_len=125):
 
     t   = tokenizer.tokenize(title)
-    q   = tokenizer.tokenize(question)
     a   = tokenizer.tokenize(answer)
     s_c = tokenizer.tokenize(sub_category)
     q_c = tokenizer.tokenize(q_class)
     q_k = tokenizer.tokenize(q_keywords)
-    qu  = tokenizer.tokenize(query)
     
     t_len   = len(t)
-    q_len   = len(q)
     a_len   = len(a)
     s_c_len = len(s_c)
     q_c_len = len(q_c)
     q_k_len = len(q_k)
-    qu_len  = len(qu)
 
-    if (t_len+q_len+a_len+4) > (max_sequence_length/2):
-        if t_max_len > t_len:
-            t_new_len = t_len
-            a_max_len = a_max_len + floor((t_max_len - t_len)/2)
-            q_max_len = q_max_len + ceil((t_max_len - t_len)/2)
-        else:
-            t_new_len = t_max_len
-      
-        if a_max_len > a_len:
-            a_new_len = a_len 
-            q_new_len = q_max_len + (a_max_len - a_len)
-        elif q_max_len > q_len:
-            a_new_len = a_max_len + (q_max_len - q_len)
-            q_new_len = q_len
-        else:
-            a_new_len = a_max_len
-            q_new_len = q_max_len
-        
-        if t_new_len+a_new_len+q_new_len+4 != max_sequence_length/2:
-            raise ValueError("New sequence length should be %d, but is %d" 
-                             % (max_sequence_length/2, (t_new_len+a_new_len+q_new_len+4)))
-        t   = t[:t_new_len]
-        q   = q[:q_new_len]
-        a   = a[:a_new_len]
-    
-    if (s_c_len+q_c_len+q_k_len+qu_len+4) > (max_sequence_length/2):
-        if s_c_max_len > s_c_len:
-            s_c_new_len = s_c_len
-            q_k_max_len = q_k_max_len + floor((s_c_max_len - s_c_len)/2)
-            qu_max_len = qu_max_len + ceil((s_c_max_len - s_c_len)/2)
-        else:
-            s_c_new_len = s_c_max_len
-        
-        if q_c_max_len > q_c_len:
-            q_c_new_len = q_c_len
-            q_k_max_len = q_k_max_len + floor((q_c_max_len - q_c_len)/2)
-            qu_max_len = qu_max_len + ceil((q_c_max_len - q_c_len)/2)
-        else:
-            q_c_new_len = q_c_max_len
-      
-        if q_k_max_len > q_k_len:
-            q_k_new_len = q_k_len 
-            qu_new_len = qu_max_len + (q_k_max_len - q_k_len)
-        elif qu_max_len > qu_len:
-            q_k_new_len = q_k_max_len + (qu_max_len - qu_len)
-            qu_new_len = qu_len
-        else:
-            q_k_new_len = q_k_max_len
-            qu_new_len = qu_max_len
+  
+    if s_c_max_len > s_c_len:
+        s_c_new_len = s_c_len
+    else:
+        s_c_new_len = s_c_max_len
+
+    if q_c_max_len > q_c_len:
+        q_c_new_len = q_c_len
+    else:
+        q_c_new_len = q_c_max_len
+
+    if q_k_max_len > q_k_len:
+        q_k_new_len = q_k_len
+    else:
+        q_k_new_len = q_k_max_len
+
+    if t_max_len > t_len:
+        t_new_len = t_len
+    else:
+        t_new_len = t_max_len
+
+    a_new_len = max_sequence_length - (t_new_len+s_c_new_len+q_c_new_len+q_k_new_len+6)
             
-        if s_c_new_len+q_c_new_len+q_k_new_len+qu_new_len+4 != max_sequence_length/2:
-            raise ValueError("New sequence length should be %d, but is %d" 
-                             % (max_sequence_length/2, (s_c_new_len+q_c_new_len+q_k_new_len+qu_new_len+4)))
-        s_c = s_c[:s_c_new_len]
-        q_c = q_c[:q_c_new_len]
-        q_k = q_k[:q_k_new_len]
-        qu  = qu[:qu_new_len]
+    if t_new_len+s_c_new_len+a_new_len+q_c_new_len+q_k_new_len+6 != max_sequence_length:
+        raise ValueError("New sequence length should be %d, but is %d" 
+                         % (max_sequence_length, (t_new_len+s_c_new_len+a_new_len+q_c_new_len+q_k_new_len+4)))
     
-    return t, q, a, s_c, q_c, q_k, qu
+    a   = a[:a_new_len]
+    t   = t[:t_new_len]
+    s_c = s_c[:s_c_new_len]
+    q_c = q_c[:q_c_new_len]
+    q_k = q_k[:q_k_new_len]
+    
+    return t, a, s_c, q_c, q_k
 
-def _convert_to_bert_inputs(title, question, answer, sub_category, q_class, q_keywords, query, tokenizer, max_sequence_length):
+def _convert_to_bert_inputs(title, answer, sub_category, q_class, q_keywords, tokenizer, max_sequence_length):
     """Converts tokenized input to ids, masks and segments for BERT"""
     
-    stoken = ["[CLS]"] + title + ["[SEP]"] + question + ["[SEP]"] + answer + ["[SEP]"] + sub_category + ["[SEP]"] + q_class + ["[SEP]"] + q_keywords + ["[SEP]"] + query + ["[SEP]"]
-
+    stoken = ["[CLS]"] + title + ["[SEP]"] + answer + ["[SEP]"] + sub_category + ["[SEP]"] + q_class + ["[SEP]"] + q_keywords + ["[SEP]"]
+    
     input_ids = _get_ids(stoken, tokenizer, max_sequence_length)
     input_masks = _get_masks(stoken, max_sequence_length)
     input_segments = _get_segments(stoken, max_sequence_length)
@@ -136,13 +107,13 @@ def _convert_to_bert_inputs(title, question, answer, sub_category, q_class, q_ke
 def compute_input_arays(df, columns, tokenizer, max_sequence_length):
     input_ids, input_masks, input_segments = [], [], []
 
-    for index, instance in tqdm(df[columns].iterrows()):
-        t, q, a, s_c, q_c, q_k, qu = instance.question_title, instance.question_body, instance.answer, instance.sub_category, instance.q_class, instance.q_keywords, instance.query
+    for _, instance in tqdm(df[columns].iterrows()):
+        t, a, s_c, q_c, q_k = instance.question_title, instance.answer, instance.sub_category, instance.q_class, instance.q_keywords
 
-        t, q, a, s_c, q_c, q_k, qu = _trim_input(t, q, a, s_c, q_c, q_k, qu, max_sequence_length)
+        t, a, s_c, q_c, q_k = _trim_input(t, a, s_c, q_c, q_k, max_sequence_length)
        
        
-        ids, masks, segments = _convert_to_bert_inputs(t, q, a, s_c, q_c, q_k, qu, tokenizer, max_sequence_length)
+        ids, masks, segments = _convert_to_bert_inputs(t, a, s_c, q_c, q_k, tokenizer, max_sequence_length)
         
         input_ids.append(ids)
         input_masks.append(masks)
